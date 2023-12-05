32315602a4772043f45ac5833d2bdc6bbe488c6f
Shaima <shooomi9988@gmail.com>
2023-12-05 16:45:38 +0400

    Add project files.

---
+++ Day15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Xml.Linq;
+
+namespace Day15
+{
+    internal class Program
+    {
+        public static void Print<T>(T val) 
+        {
+            Console.WriteLine(typeof(T));
+            Console.WriteLine(val);
+        }
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[]
+            {
+                new Employee{Id = 1 , Name = "Shaima" , Gender = 'f', TotalSales =299999m },
+                new Employee{Id = 2, Name = "Ali" , Gender = 'm', TotalSales =29999m},
+                 new Employee{Id = 3, Name = "Bader" , Gender = 'm', TotalSales =9000000m }
+            };
+            Report report = new Report();
+            report.TotalSalesGreaterThan5000(employees);
+            report.TotalSalesGreaterThan5000LessThan3000(employees);
+            report.TotalSalesGreaterLessThan30000(employees);
+
+            report.TotalSales(employees, "Total Sales Greater Than 5000", (emp) => emp.TotalSales > 50000);
+
+            Print("Rabab");
+            Print(9);
+            Print('S');
+
+
+        }
+    }
+}
---
+++ Day15/Report.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    internal class Report
+    {
+        public delegate bool salesConditions(Employee emp);
+        public void TotalSalesGreaterThan5000(Employee[] em)
+        {
+            Console.WriteLine("Total Sales Greater Than 5000");
+            Console.WriteLine("__________________________________________________\n");
+            foreach (Employee emp in em)
+                if (emp.TotalSales > 50000)
+                    Console.WriteLine($"{emp.Id} | {emp.Name} | {emp.Gender} | {emp.TotalSales}");
+        }
+
+        public void TotalSalesGreaterThan5000LessThan3000(Employee[] em)
+        {
+            Console.WriteLine("\nTotal Sales Greater Than 5000 Less Than 3000");
+            Console.WriteLine("__________________________________________________\n");
+
+            foreach (Employee emp in em)
+                if (emp.TotalSales <= 50000 && emp.TotalSales >= 30000)
+                    Console.WriteLine($"{emp.Id} | {emp.Name} | {emp.Gender} | {emp.TotalSales}");
+        }
+
+        public void TotalSalesGreaterLessThan30000(Employee[] em)
+        {
+            Console.WriteLine("\nTotal Sales Greater Less Than 30000");
+            Console.WriteLine("__________________________________________________\n");
+
+            foreach (Employee emp in em)
+                if (emp.TotalSales < 30000)
+                    Console.WriteLine($"{emp.Id} | {emp.Name} | {emp.Gender} | {emp.TotalSales}");
+            Console.WriteLine();
+        }
+
+        public void TotalSales(Employee[] emp, string title, salesConditions conditions) 
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("____________________________________");
+            foreach (Employee employee in emp)
+            {
+                if (conditions(employee)) 
+                {
+                    Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.Gender} | {employee.TotalSales}");
+                }
+            }
+        }
+    }
+}
